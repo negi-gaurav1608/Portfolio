11f098032da62617beeab69c9a163a1c61d0dc9e
negi-gaurav1608 <ngaurav825@gmail.com>
2024-08-01 22:44:37 +0530

    portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,13 +9,13 @@ const About = () => {
                     About Me
                 </h1>
                 </div>
-                <p>
+                <p className='pt-4 text-xl text-gray-900'>
                 As a dedicated frontend developer and C++ coder, I have a strong foundation in both web development and algorithmic problem-solving. I've created dynamic and user-friendly web interfaces using HTML, CSS, JavaScript, and modern frameworks like React, featuring an intuitive user interface and seamless navigation.
                 </p>
-                <p>
+                <p className='py-6 text-xl text-gray-900'>
                 In addition to my frontend expertise, I have solved over 200 questions on LeetCode and 100 questions on GeeksforGeeks, showcasing my proficiency in data structures and algorithms. My programming skills extend to languages like Java and Python, which I have used for various projects and competitive programming.
                 </p>
-                <p>
+                <p className='pb-4 text-xl text-gray-900'>
                 These experiences highlight my versatility and commitment to continuous learning. I prioritize clean code, accessibility, and responsiveness in all my projects, ensuring an optimal user experience across all devices. Each project and coding challenge has enhanced my problem-solving abilities and attention to detail.
                 </p>
             </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,12 +10,12 @@ function Projects() {
     return (
         <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
             <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
-            <p className='text-center pt-8'>As a passionate frontend developer, I've dedicated my time to creating dynamic and user-friendly web interfaces. My projects span various domains and showcase my skills in HTML, CSS, JavaScript, and modern frameworks like ReactJs.</p>
-            <p className='text-center py-2'>
+            <p className='text-center pt-8 text-xl'>As a passionate frontend developer, I've dedicated my time to creating dynamic and user-friendly web interfaces. My projects span various domains and showcase my skills in HTML, CSS, JavaScript, and modern frameworks like ReactJs.</p>
+            <p className='text-center py-2 text-xl'>
                 One of my notable projects is a responsive website designed with Tailwind CSS, featuring an intuitive user interface and seamless navigation.
             </p>
             
-            <p className='text-center pb-8'>
+            <p className='text-center pb-10 text-xl'>
                 These projects highlight my ability to translate design concepts into functional, aesthetically pleasing web pages. I prioritize clean code, accessibility, and responsiveness, ensuring an optimal user experience across all devices. Each project has provided valuable learning experiences and has enhanced my problem-solving skills and attention to detail.
             </p>
             
